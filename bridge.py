89f9ee5b7b8789fdc346f483650936c92d749115
Aaron Lio <bossaaron3@MacBook-Air-100.local>
2021-11-29 12:32:10 -0500

    finalized bridge script

--- bridge.py
+++ bridge.py
@@ -1,4 +1,4 @@
-
+from itertools import product
 import math as math
 import random as random
 from numpy.core.records import recarray
@@ -17,10 +17,11 @@ pi = math.pi
 
 ###CONSTRAINTS###
 mandatory_length = 1280
+split_point = 798 #from right to left: go from cross section A to B 
 total_matboard = 813*1016 #826008
 
 class Bridge:
-    def __init__(self, height, length, glue_tab_width, num_top_flange_layers, num_bottom_flange_layers, num_web_layers, web_dist):
+    def __init__(self, height, length, glue_tab_width, num_top_flange_layers, num_bottom_flange_layers, num_web_layers, web_dist, dia_dist, dia_num):
         self.paper_thickness = 1.27
         self.height = height
         self.length = length
@@ -31,9 +32,12 @@ class Bridge:
         self.glue_tab_width = glue_tab_width
         self.glue_tab_thickness = self.paper_thickness
         self.web_dist = web_dist
+        self.dia_num=dia_num
         self.top_flange_thickness = self.num_top_flange_layers*self.paper_thickness
         self.bottom_flange_thickness = self.num_bottom_flange_layers*self.paper_thickness
         self.web_thickness = num_web_layers*self.paper_thickness
+        self.dia_dist = dia_dist
+        self.I = self.get_I_A()
 
     def SFD_train(self):
         wheel_positions = [0, 176, 340, 516, 680, 856]
@@ -160,7 +164,8 @@ class Bridge:
         bottom_flange_thickness = self.bottom_flange_thickness
         web_thickness = self.web_thickness
         
-        I = self.get_I_A()
+        self.I = self.get_I_A()
+        I = self.I
         y = self.ybar()
 
         self.ytop = height - y
@@ -200,14 +205,14 @@ class Bridge:
         web_thickness = self.web_thickness
         
         
-        I = self.get_I_A()
+        self.I = self.get_I_A()
+        I = self.I
         y = self.ybar()
         
         Q = ((web_thickness*2)*(y-bottom_flange_thickness)*(y-abs(y-bottom_flange_thickness)/2)) + (bottom_flange_thickness*web_dist)*abs(y-(bottom_flange_thickness/2))
         P5 = (max_shear*I*(2*web_thickness))/Q
 
-        self.Qglue = (top_flange_thickness*flange_width)*(height - (top_flange_thickness/2) - y)
-        Qglue = self.Qglue
+        Qglue = (top_flange_thickness*flange_width)*(height - (top_flange_thickness/2) - y)
         P6 = (max_shear_cement*I*(self.glue_tab_width))/Qglue
 
         return [P5,P6]
@@ -230,7 +235,6 @@ class Bridge:
 		#Case 1: Compressive flange at top
         sig_comp_crit1 = ((4*(pi**2)*E)/(12*(1-mu**2)))*((top_flange_thickness/web_dist)**2)
         P7 = (sig_comp_crit1*I)/(166*self.ytop)
-
 
 		#Case 2: Flexural compression @ top of web
         sig_comp_crit2 = ((6*(pi**2)*E)/(12*(1-mu**2)))*(( web_thickness /(height-y-top_flange_thickness-self.glue_tab_thickness))**2)
@@ -271,19 +275,29 @@ class Bridge:
 
     # get FOS's, use calculations
     def FOS(self):
+        height = self.height
+        flange_width = self.flange_width
+        web_dist = self.web_dist
+        length = self.length
+        top_flange_thickness = self.top_flange_thickness
+        bottom_flange_thickness = self.bottom_flange_thickness
+        web_thickness = self.web_thickness
+        y = self.ybar()
+
+        ytop = height - y
+        ybot = y
+
         sigma_crit_case_1 = 3.455
         sigma_crit_case_2 = 23.5
         sigma_crit_case_3 = 31.864
         sigma_crit_case_4 = 3.45
+        Q = ((web_thickness*2)*(y-bottom_flange_thickness)*(y-abs(y-bottom_flange_thickness)/2)) + (bottom_flange_thickness*web_dist)*abs(y-(bottom_flange_thickness/2))
         I = self.I
-        Q = self.Q
         b = self.paper_thickness
-        Qglue = self.Qglue
+        Qglue = (top_flange_thickness*flange_width)*(height - (top_flange_thickness/2) - y)
         
         # Scenario 1 - Train is between supports
         M_plus_max = 54830
-        ytop = self.ytop
-        ybot = self.ybot
         FOSc = 6/((54830*ytop)/I)
         FOSt = 30/((54830*ybot)/I)
         shearFOS = (4/((200*Q)/(I*(2*b))))
@@ -314,14 +328,34 @@ class Bridge:
 
         FOSes = [FOSc, FOSt, shearFOS, shearGlueFOS,bucklingFOSCase1, bucklingFOSCase2, bucklingFOSCase3, bucklingFOSCase4, 
         FOSc2a, FOSt2a, FOSc2b, FOSt2b, shearFOS2, shearGlueFOS2, bucklingFOSCase1_2, bucklingFOSCase2_2, bucklingFOSCase3_2, bucklingFOSCase4_2]
-
+        
+        #print("I-value\n\n\n", self.I)
         for num in FOSes:
             if num < 1:
                 return False
         
         return True
 
-
+    def deadLoad(self):
+        failure_modes = {
+            "Tension failure at bottom": self.get_max_P_flexural_A()[0], 
+            "Tension failure at top": self.get_max_P_flexural_A()[1], 
+            "Compressive failure at bottom": self.get_max_P_flexural_A()[2], 
+            "Compressive failure at top": self.get_max_P_flexural_A()[3],
+            "Matboard Shear Failure": self.get_max_P_shear_A()[0],
+            "Glue Shear Failure": self.get_max_P_shear_A()[1],
+            "Buckling Failure of Top Flange at Mid": self.get_buckling_failure_A()[0],
+            "Buckling Failure of Top of Web": self.get_buckling_failure_A()[1],
+            "Buckling Failure of Bottom of Web": self.get_buckling_failure_A()[2],
+            "Buckling Failure of Top Flange at ends": self.get_buckling_failure_A()[3],
+            "Buckling Failure of bottom flange": self.get_buckling_failure_A()[4],
+            "Shear Buckling Failure of Top of Web with a = 520": self.get_buckling_failure_A()[5],
+            "Shear Buckling Failure of Top of Web with a = 480": self.get_buckling_failure_A()[6]}
+        failure_load = min(zip(failure_modes.values(), failure_modes.keys()))[1]
+        return failure_modes[failure_load]
+    def deadTrain(self):
+        return self.FOS()
+        
     def report(self):
         failure_modes = {
             "Tension failure at bottom": self.get_max_P_flexural_A()[0], 
@@ -333,17 +367,68 @@ class Bridge:
             "Buckling Failure of Top Flange at Mid": self.get_buckling_failure_A()[0],
             "Buckling Failure of Top of Web": self.get_buckling_failure_A()[1],
             "Buckling Failure of Bottom of Web": self.get_buckling_failure_A()[2],
-            "Buckling Failure of Top of Web": self.get_buckling_failure_A()[3],
-            "Buckling Failure of Top Flange at ends": self.get_buckling_failure_A()[4],
-            "Buckling Failure of bottom flange": self.get_buckling_failure_A()[5],
-            "Shear Buckling Failure of Top of Web with a = 520": self.get_buckling_failure_A()[0],
-            "Shear Buckling Failure of Top of Web with a = 480": self.get_buckling_failure_A()[1]}
-            
+            "Buckling Failure of Top Flange at ends": self.get_buckling_failure_A()[3],
+            "Buckling Failure of bottom flange": self.get_buckling_failure_A()[4],
+            "Shear Buckling Failure of Top of Web with a = 520": self.get_buckling_failure_A()[5],
+            "Shear Buckling Failure of Top of Web with a = 480": self.get_buckling_failure_A()[6]}
+        
+        #for the point load test
+        failure_load = min(zip(failure_modes.values(), failure_modes.keys()))[1]
+        print(f"The way to fail the bridge is: {failure_load}")
+        print(f"The maximun load that will fail the bridge is: {failure_modes[failure_load]}")
+        #for the train test
         FOSes = self.FOS()
-        print(FOSes)
+        print(f"Pass the train test: {FOSes}")
+    
+    def get_amount_paper(self):
+        height = self.height
+        flange_width = self.flange_width
+        web_dist = self.web_dist
+        top_flange_thickness = self.top_flange_thickness
+        bottom_flange_thickness = self.bottom_flange_thickness
+        web_thickness = self.web_thickness
+        length = self.length
+        dia_dist = self.dia_dist
+        dia_num=self.dia_num
+        num_flange_layers_top = self.num_top_flange_layers
+        num_flange_layers_bottom = self.num_bottom_flange_layers
+        num_web_layers = self.num_web_layers
+        glue_tab_width=self.glue_tab_width
+        paper_used=0
+        top_layer_A = (flange_width*length*num_flange_layers_top)
+        each_side_layer_A=(num_web_layers*(height-top_flange_thickness-bottom_flange_thickness+2*glue_tab_width)*length)
+        #with glue tab!!
+        bottom_layer_A=web_dist*length*num_flange_layers_bottom
+        paper_used+=(top_layer_A+2*each_side_layer_A+bottom_layer_A)
+        num_diaphragms = (length/dia_dist)+5
+        area_dia = dia_num*(height-top_flange_thickness-bottom_flange_thickness)*web_dist
+        
+        paper_used += area_dia
+        
+        return paper_used
+
+#self, height, length, glue_tab_width, num_top_flange_layers, num_bottom_flange_layers, num_web_layers, web_dist, dia_dist, dia_num
 
 
-
-
-b1 = Bridge(75, 1280, 11.27, 1, 1, 1, 80)
+b1 = Bridge(75, 1280, 11.27, 2, 1, 1, 80, 550, 8)
 b1.report()
+b2=Bridge(97, 1280,19,1, 3, 1,61, 260,16)
+print(f"AREAAA: {b2.get_amount_paper()/(813*1016)}")
+print(f"train: {b2.deadTrain()}")
+b2.report()
+"""
+lllll=[0,0,0,0,0, 0, 0]#min load, x, y, z,glueW, height, i
+print(b1.get_amount_paper())
+for x,y,z, glueW, height in product(range(1, 5), range(1, 5), range(1, 5), range(8,20), range(50,100)):
+    for i in range (1,100):
+        b_sample=Bridge(height, 1280, glueW, x,y,z, i, 520, 8)
+        if (b_sample.deadLoad()>lllll[0] and b_sample.get_amount_paper()<813*1016*0.92 and b_sample.deadTrain()):
+            lllll[0]=b_sample.deadLoad()
+            lllll[1]=x
+            lllll[2]=y
+            lllll[3]=z
+            lllll[4]=glueW
+            lllll[5]=height
+            lllll[6]=i
+print (f"The size is: {lllll}")
+"""
